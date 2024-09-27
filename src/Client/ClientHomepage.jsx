ab3265763ae2f90dc77a45326ce44984ab659124
Monica <bosleyed20@gmail.com>
2024-09-27 14:14:11 +0800

    fourth commit

--- src/Client/ClientHomepage.jsx
+++ src/Client/ClientHomepage.jsx
@@ -11,12 +11,12 @@ const slides = [
 export default function SewiftHomepage() {
     
     return (
-        <div className='bg-[#d9edf4] min-h-screen'>
+        <div>
             <div>
             <ClientHeader />
             </div>
             
-            <div className='bg-[#fefefe] min-h-screen flex py-10 justify-center'> 
+            <div className='bg-[#fefefe] flex py-5 justify-center'> 
             <div className='max-w-6xl'>
                 <Carousel autoSlide={true} autoSlideInterval={5000}>
                     {slides.map((s) => (
@@ -24,6 +24,29 @@ export default function SewiftHomepage() {
                     ))}
                 </Carousel>
             </div>
+            </div>
+
+            <div className='bg-[#ffc696]'>
+                <div className='grid grid-cols-3 gap-7 p-10 flex justify-center'>
+                    <div>
+                    <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">View Order Progress</h5>
+                        <p class="font-normal text-gray-700 dark:text-gray-400">You currently have no orders in progress.</p>
+                        </a>
+                    </div>
+                    <div>
+                    <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">View Complete Orders</h5>
+                        <p class="font-normal text-gray-700 dark:text-gray-400">You currently have no orders.</p>
+                        </a>
+                    </div>
+                    <div>
+                    <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">View Tailor Shops</h5>
+                        <p class="font-normal text-gray-700 dark:text-gray-400">Check out tailor shops and their services.</p>
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
     )
