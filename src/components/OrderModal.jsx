9bf1f7c4feab3816a1367390ea3c12fb7033902c
Monica <bosleyed20@gmail.com>
2024-10-09 16:16:57 +0800

    eleventh commit

--- src/components/OrderModal.jsx
+++ src/components/OrderModal.jsx
@@ -156,7 +156,7 @@ export default function OrderModal() {
                                                 name="remarks"
                                                 rows="4"
                                                 className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                                placeholder="Enter any custom requests here"
+                                                placeholder="Enter your remarks here"
                                             ></textarea>
                                         </div>
                                     </div>
