e7f093c625da398e7b032c9f217e2e07cc66e6a5
Monica <bosleyed20@gmail.com>
2024-10-16 00:05:47 +0800

    13th commit

--- src/Admin/AdminShopProfile.jsx
+++ src/Admin/AdminShopProfile.jsx
@@ -1,40 +1,44 @@
 import React from 'react';
 import AdminNav from '/src/components/AdminNav'
 import Upload from '/src/components/Upload'
+import Input from '/src/components/Input'
 
 export default function AdminShopProfile() {
     return (
-        <div className='grid grid-cols-5 bg-[#3f6e85]'>
+        <div className='grid grid-cols-4 bg-[#3f6e85]'>
             <AdminNav />
 
-            <div className='col-span-4 bg-[#f0f0f0]'>
+            <div className='col-span-3 bg-[#f0f0f0]'>
                 <div className='m-5 p-5 bg-[#fefefe] shadow'>
-                    <div className='flex'>
-                        <Upload />
-                        <div className='mx-3 '>
+                    <div className='flex items-center justify-between'>
+                        <div className='relative flex'>
+                            <Upload />
+                        <div className='mx-3'>
                             <h1 className='text-lg font-semibold'>Tailor Shop Name</h1>
                             <p>Working Hours</p>
                             <p>Open from Monday - Friday</p>
                             <p>Complete Address</p>
+                            <Input />
+                        </div>
+                        </div>
+
+                        <div className='flex justify-end'>
+                            <button>
+                                Edit
+                            </button>
                         </div>
                     </div>
 
-                    <div class="mt-6 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                        <a href="#">
-                            <img class="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
-                        </a>
-                    <div class="p-5">
-                        <a href="#">
-                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                        </a>
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                            Read more
-                            <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                            </svg>
-                        </a>
-                    </div>
+                    <div className='mt-10 flex block max-w-xs p-3 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700'>
+                        <div>
+                            <Upload />
+                        </div>
+                        <div className='pl-2'>
+                            <h1 className='text-lg font-semibold'>Product Name</h1>
+                            <p>Description</p>
+                            <p>Size</p>
+                            <p>Price</p>
+                        </div>
                     </div>
                 </div>
             </div>
---
+++ src/components/Input.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Flex, Input } from 'antd';
+const { TextArea } = Input;
+const onChange = (e) => {
+  console.log('Change:', e.target.value);
+};
+const App = () => (
+    <TextArea 
+    showCount
+    maxLength={100} 
+    onChange={onChange} 
+    placeholder="Description" 
+    className='mt-3 custom-width max-w-lg' 
+  />
+);
+export default App;
