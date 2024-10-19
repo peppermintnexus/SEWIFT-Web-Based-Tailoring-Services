61a111ac468d55799c9032bbc4dbf3d4a651b209
Monica <bosleyed20@gmail.com>
2024-10-19 16:19:23 +0800

    fifth commit

--- src/components/JobOrderModal.jsx
+++ src/components/JobOrderModal.jsx
@@ -1,27 +1,41 @@
 import React, { useState } from 'react';
 import { Button, Modal } from 'antd';
+
 const App = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+
   const showModal = () => {
     setIsModalOpen(true);
   };
-  const handleOk = () => {
-    setIsModalOpen(false);
-  };
+
   const handleCancel = () => {
     setIsModalOpen(false);
   };
+
   return (
     <>
       <Button type="primary" onClick={showModal}>
         View More
       </Button>
-      <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
+      {/* Remove OK and Cancel buttons by setting footer to null */}
+      <Modal title="JO Number 12345" open={isModalOpen} onCancel={handleCancel} footer={null}>
+        <p>Client Name:</p>
+        <p>Tailor ID:</p>
+        <p>Date of Order:</p>
+        <p>Quantity:</p>
+        <p>Total Price:</p>
+        <p>Remarks:</p>
+
+        <div className='flex items-center'>
+          <img 
+            src="https://via.placeholder.com/150"
+            alt="Placeholder"
+            className="w-auto h-auto object-cover rounded-lg shadow-md" 
+          />
+        </div>
       </Modal>
     </>
   );
 };
+
 export default App;
