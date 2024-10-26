b546c82158a6aac17672466445bb4020337fc2fc
Monica <bosleyed20@gmail.com>
2024-10-26 13:06:11 +0800

    15th commit

--- src/Admin/AdminTransactionHistory.jsx
+++ src/Admin/AdminTransactionHistory.jsx
@@ -1,26 +1,43 @@
 import React from 'react';
 import AdminSidebar from '/src/components/AdminSidebar'
-import Upload from '/src/components/Upload'
+import JobOrderModal from '/src/components/JobOrderModal'
 
-export default function AdminShopProfile() {
+export default function AdminTransactionHistory() {
     return (
-        <div className='grid grid-cols-5 bg-[##f7f7f7]'>
+        <div className='grid grid-cols-5 bg-[#f7f7f7] min-h-screen'>
             <AdminSidebar />
 
             <div className='col-span-4 bg-[#f7f7f7] p-4'>
-                    <h1 className='font-semibold text-3xl'>Transaction History</h1>
+                    <div className=''>
+                        <h1 className='font-semibold text-3xl px-5 py-5'>Transaction History</h1>
 
-                    <div className='bg-[#fefefe] mt-5 p-3'>
-                        <div className='flex'>
-                            <Upload />
+                        <div className='grid grid-cols-2 p-5'>
+                        <div className='bg-[#fefefe] rounded-lg shadow-md p-3'>
+                            <div className='flex justify-between items-center'>
+                            <h1 className='font-semibold'>
+                                JO Number 12345
+                            </h1>
+                            <JobOrderModal />
+                            </div>
+                            <hr className="border-t border-gray-200 my-3" />
 
-                            <div className='pl-3'>
-                            <h1 className='text-lg font-semibold'>Product Name</h1>
-                            <p>Type of Order</p>
+                            <div className='flex items-center'>
+                            <img src="https://via.placeholder.com/150"
+                            alt="Placeholder"
+                            className="w-32 h-32 object-cover rounded-lg shadow-md" />
+                            
+                            <div className='pl-4'>
+                                <h1 className='font-semibold'>Product Name</h1>
+                                <ol className='pt-4'>
+                                    <li>Type of Order:</li>
+                                    <li>Client Name:</li>
+                                    <li>Order Date:</li>
+                                </ol>
+                            </div>
                             </div>
                         </div>
-                        
                     </div>
+                    </div>  
             </div>
         </div>
     )
