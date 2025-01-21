482607e761892f5709008160ac268ba65ec05ce6
Monica <bosleyed20@gmail.com>
2025-01-21 16:02:49 +0800

    4th

--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function ClientProfile() {
     return (
-        <div className='bg-[#fefefe]'>
+        <div className='bg-[#fefefe] min-h-screen'>
             <div className='h-full'>
-            <div className=' m-2 pl-4 py-2 flex items-center gap-5'>
+            <div className='pl-4 py-2 flex items-center gap-5'>
             <a href='/EmployeeHomepage'>
             <svg fill="#000000" className='w-12 h-12 hover:bg-[#f7f7f7] py-2 pr-1 rounded-full' viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
             <g data-name="arrow left" id="arrow_left">
@@ -12,48 +12,50 @@ export default function ClientProfile() {
             </g>
             </svg>
             </a>
-                <h1 className='text-xl'>Add Product</h1>
+                <h1 className='text-xl'>New Product</h1>
             </div>
 
-            <div className='bg-[#EBEBEB] px-10 pt-7'>
-                <div className='py-3 px-5 border bg-[#fefefe] space-y-1'>
-                    <p>Name</p>
-                    <input className='px-1.5 py-1 border w-full'>
-                    </input>
+            <div className='bg-[#EBEBEB] px-10 py-7'>
+                <div className='grid grid-cols-2 gap-3'>
+                    <div className='space-y-1 p-4 bg-[#fefefe]'>
+                        <p>Name</p>
+                        <input className='w-full px-1.5 py-1 text-gray-900 bg-gray-50 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' />
 
-                    <p>Category</p>
-                    <input className='px-1.5 py-1 border w-full'>
-                    </input>
+                        <p>Category</p>
+                        <input className='w-full px-1.5 py-1 text-gray-900 bg-gray-50 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' />
 
-                    <p>Description</p>
-                    <input className='px-1.5 py-1 border w-full'>
-                    </input>
+                        <p>Description</p>
+                            <textarea id="message" rows="4" class="block px-1.5 py-1 w-full text-sm text-gray-900 bg-gray-50 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 resize-none" placeholder="Apparel description"></textarea>
+
+
+                    </div>
+                    <div className='space-y-1 p-4 bg-[#fefefe]'>
+                        <p>Status</p>
+                        <input className='w-full px-1.5 py-1 text-gray-900 bg-gray-50 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' />
+                    </div>
                 </div>
 
-                <div className='mt-5 py-3 px-5 border bg-[#fefefe] space-y-1'>
-                    <p className='text-xl pb-4'>Properties</p>
-
-                    <div className='grid grid-cols-2 gap-4 '>
+                <div className='bg-[#fefefe] space-y-1 p-4 mt-3'>
+                    <p className='text-xl mb-3 font-medium'>Properties</p>
+                    
+                    <div className='grid grid-cols-2 gap-3'>
                         <div className='space-y-1'>
                             <p>Price</p>
-                            <input className='px-1.5 py-1 border w-full'>
-                            </input>
-                        </div>
-                        <div className='space-y-1'>
-                            <p>Stock</p>
-                            <input className='px-1.5 py-1 border w-full'>
-                            </input>
+                            <input className='w-full px-1.5 py-1 text-gray-900 bg-gray-50 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' />
+
+                            <p>Size</p>
+                            <input className='w-full px-1.5 py-1 text-gray-900 bg-gray-50 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' />
                         </div>
 
                         <div className='space-y-1'>
-                            <p>Size</p>
-                            <input className='px-1.5 py-1 border w-full'>
-                            </input>
-                        </div>
-                        <div className='space-y-1'>
-                            <p>Status</p>
-                            <input className='px-1.5 py-1 border w-full'>
-                            </input>
+                            <p>Stock</p>
+                            <input className='w-full px-1.5 py-1 text-gray-900 bg-gray-50 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' />
+
+                            <div className='grid justify-items-end pt-6'>
+                            <button 
+                            type="button" 
+                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Add Product</button>
+                            </div>
                         </div>
                     </div>
                 </div>
