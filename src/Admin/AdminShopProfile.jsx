31487fa94832ac084333df7fa71d045816e17d1c
Monica <bosleyed20@gmail.com>
2025-01-24 01:25:52 +0800

    8th

--- src/Admin/AdminShopProfile.jsx
+++ src/Admin/AdminShopProfile.jsx
@@ -16,12 +16,24 @@ export default function AdminShopProfile() {
                     <div className='grid grid-cols-5 bg-[#Fefefe]'>
                         <AdminSidebar />
         
-                        <div className='ml-2 p-5 col-start-2 col-end-6'>
+                        <div className='ml-2 px-2 py-5 col-start-2 col-end-6'>
 
                                 <div className='flex justify-between col-start-2 col-end-6'>
 
                                     <div className='ml-4 flex'>
-                                    <img class="object-cover h-40 w-40" src={Sample} alt="image description" />
+                                    <div class="items-center justify-center w-full">
+                                        <label for="dropzone-file" class="flex flex-col items-center justify-center w-48 h-48 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                                            <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                                                <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
+                                                </svg>
+                                                <p class="mb-2 text-xs text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
+                                                <p class="text-center text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+                                            </div>
+                                                <input id="dropzone-file" type="file" class="hidden" />
+                                                </label>
+                                          </div>
+
                                         <div className='ml-4'>
                                         <h1 className='text-xl font-medium'>Tailor Shop Name</h1>
                                         <p className='mb-2 text-sm text-[#9C9C9C]'>Rating</p>
@@ -32,7 +44,9 @@ export default function AdminShopProfile() {
                                     </div>
 
                                     <div className='justify-self-end'>
+                                        <a href='/AddProduct'>
                                             <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 ">Add Product</button>
+                                        </a>
                                             <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 ">Edit</button>                                    
                                     </div>
                                 </div>
--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -20,7 +20,7 @@ export default function ClientProfile() {
         <div className='bg-[#fefefe] min-h-screen'>
             <div className='h-full'>
                 <div className='pl-4 py-2 flex items-center gap-5'>
-                    <a href='/EmployeeHomepage'>
+                    <a href='/AdminShopProfile'>
                         <svg fill="#000000" className='w-12 h-12 hover:bg-[#f7f7f7] py-2 pr-1 rounded-full' viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
                             <g data-name="arrow left" id="arrow_left">
                                 <path className="cls-1" d="M22,29.73a1,1,0,0,1-.71-.29L9.93,18.12a3,3,0,0,1,0-4.24L21.24,2.56A1,1,0,1,1,22.66,4L11.34,15.29a1,1,0,0,0,0,1.42L22.66,28a1,1,0,0,1,0,1.42A1,1,0,0,1,22,29.73Z"/>
